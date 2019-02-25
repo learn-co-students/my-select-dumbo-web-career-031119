f050131f83e37f2de6a52453c506aa780c7b8ace
Jordan R <jordan.rivera34@gmail.com>
2019-02-25 14:31:34 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+ nuarray =[]
+ i = 0
+
+ while i < array.length
+   if yield(array[i]) == true
+     nuarray << array[i]
+   end
+  i+=1
+  end
+nuarray
 end
